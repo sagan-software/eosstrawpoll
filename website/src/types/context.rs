9bf72a00a9750ebfd4de87f9f70113598f3b55c0
Liam Curry <liam@sagan.software>
2018-09-06 17:11:10 -0400

    added kylin

--- website/src/types/context.rs
+++ website/src/types/context.rs
@@ -37,13 +37,8 @@ impl Default for Context {
 
         Context {
             lang: lang.parse().unwrap_or_else(|_| Lang::English),
-            selected_chain: eos_devnet(),
-            available_chains: vec![
-                eos_testnet_jungle(),
-                eos_devnet(),
-                telos_devnet(),
-                telos_testnet(),
-            ],
+            selected_chain: eos_testnet_jungle(),
+            available_chains: vec![eos_testnet_jungle(), kylin_testnet(), telos_testnet()],
         }
     }
 }
